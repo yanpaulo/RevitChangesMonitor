814088bda0e9f7d8bb6784854d067c2614d79ed6
yanpa <yanpa@YAN-NOTE>
2017-03-08 13:11:09 -0300

    Add project files.

---
+++ RevitChangesMonitor/ChangesInfoForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿//
+// (C) Copyright 2003-2016 by Autodesk, Inc.
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE. AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
+
+namespace Revit.ChangesMonitor
+{
+    partial class ChangesInformationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.changesdataGridView = new System.Windows.Forms.DataGridView();
+            this.topMostCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.changesdataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // changesdataGridView
+            // 
+            this.changesdataGridView.AllowUserToAddRows = false;
+            this.changesdataGridView.AllowUserToDeleteRows = false;
+            this.changesdataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.changesdataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.changesdataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.changesdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.changesdataGridView.Location = new System.Drawing.Point(0, 0);
+            this.changesdataGridView.Name = "changesdataGridView";
+            this.changesdataGridView.ReadOnly = true;
+            this.changesdataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.changesdataGridView.Size = new System.Drawing.Size(662, 235);
+            this.changesdataGridView.TabIndex = 0;
+            this.changesdataGridView.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.changesdataGridView_RowsAdded);
+            // 
+            // topMostCheckBox
+            // 
+            this.topMostCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.topMostCheckBox.AutoSize = true;
+            this.topMostCheckBox.Location = new System.Drawing.Point(12, 241);
+            this.topMostCheckBox.Name = "topMostCheckBox";
+            this.topMostCheckBox.Size = new System.Drawing.Size(67, 17);
+            this.topMostCheckBox.TabIndex = 1;
+            this.topMostCheckBox.Text = "T&opmost";
+            this.topMostCheckBox.UseVisualStyleBackColor = true;
+            this.topMostCheckBox.CheckedChanged += new System.EventHandler(this.topMostCheckBox_CheckedChanged);
+            // 
+            // ChangesInformationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 267);
+            this.Controls.Add(this.topMostCheckBox);
+            this.Controls.Add(this.changesdataGridView);
+            this.MaximizeBox = false;
+            this.Name = "ChangesInformationForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Changes Information";
+            this.Shown += new System.EventHandler(this.ChangesInfoForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.changesdataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView changesdataGridView;
+        private System.Windows.Forms.CheckBox topMostCheckBox;
+    }
+}
