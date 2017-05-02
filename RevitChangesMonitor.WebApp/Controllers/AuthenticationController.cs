7aef614cfa7e4e5c76da73119a494e042d905ac7
Yan Paulo <yanpaulo@hotmail.com>
2017-05-02 14:24:04 -0300

    Created Users database. Added Basic Authentication support for Identity. Added Authentication api Controller.

---
+++ RevitChangesMonitor.WebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using RevitChangesMonitor.WebApp.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RevitChangesMonitor.WebApp.Controllers
+{
+    [IdentityBasicAuthentication]
+    [Authorize]
+    public class AuthenticationController : ApiController
+    {
+        [Route("api/Authentication/")]
+        public IHttpActionResult DoTest() => Ok();
+
+    }
+}
